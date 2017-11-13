5e0c93904ed8c180ec1c8e097ff677c051640bda
Reinaldo Mizutani <30627739+reinaldomizutani@users.noreply.github.com>
2017-11-13 17:57:46 -0200

    Add files via upload

--- icc2/buda/main.c
+++ icc2/buda/main.c
@@ -1,19 +1,31 @@
 #include "trab.c"
+#include "quick.c"
 
 int main(){
+	//-----------------------------------------------------
+	//1. declaracao de variaveis
+	//-----------------------------------------------------
+
     FILE *fp;
     fp = fopen("paises.txt", "r");
 	char pais[30];
+	char chaveChar[30];
 	long gdp, pop, area;
-	
-	int i=0, j, contArea=0, op, contGdp=0 , contPop=0;
-	
+	long chavePop, chaveArea, chaveGdp;
+	int tam, aux=0, contArea=0, contPop=0, contGdp=0;
+	int i=0, j;
+	clock_t tempoInicial, tempoFinal;
+	double tempoTotal;
 	no *paises = NULL;
-	
-	
+	srand(time(NULL));
+
+	//-----------------------------------------------------
+	//2. leitura de dados do arquivo
+	//-----------------------------------------------------
+
 	while(!feof(fp)){
 		fscanf(fp, "%s %ld %ld %ld", pais, &area, &pop, &gdp);
-		
+
 		paises = (no*)realloc(paises, (i+1)*sizeof(no));
 		strcpy(paises[i].pais, pais);
 		paises[i].area = area;
@@ -21,22 +33,144 @@ int main(){
 		paises[i].gdp = gdp;
 		i++;
 	}
+	tam = i;
+	//-----------------------------------------------------
+	//3. chamadas das funcoes de ordenacao
+	//-----------------------------------------------------
+	system("cls");
+	//3.b - Quick Sort
+	//POPULACAO
+	printf("2.a.a - Quick Sort - Populacao (pivo meio):\n");
+	tempoInicial = clock();
+	quickSortPivoMeioPop(paises,0,tam,&aux);
+	tempoFinal = clock();
+	tempoTotal = (tempoFinal - tempoInicial)/(CLOCKS_PER_SEC);	
+	printf("Tempo de ordenacao: %.12f\nnumero de operacoes:%d\n--\n", tempoTotal, aux);
+	aux = 0;
+
+	printf("2.a.b -  Quick Sort - Populacao (pivo aleatorio):\n");
+	tempoInicial = clock();
+	quickSortPivoAleatorioPop(paises,0,tam,&aux);
+	tempoFinal = clock();
+	tempoTotal = (tempoFinal - tempoInicial)/(CLOCKS_PER_SEC);	
+	printf("Tempo de ordenacao: %.12f\nnumero de operacoes:%d\n--\n", tempoTotal, aux);
+	aux = 0;
+
+	printf("2.a.c -  Quick Sort - Populacao (pivo mediana):\n");
+	tempoInicial = clock();
+	quickSortPivoMedianaPop(paises,0,tam,&aux);
+	tempoFinal = clock();
+	tempoTotal = (tempoFinal - tempoInicial)/(CLOCKS_PER_SEC);	
+	printf("Tempo de ordenacao: %.12f\nnumero de operacoes:%d\n--\n", tempoTotal, aux);
+	aux = 0;
+
+	//AREA
+	printf("2.b.a -  Quick Sort - Area (pivo meio):\n");
+	tempoInicial = clock();
+	quickSortPivoMeioArea(paises,0,tam,&aux);
+	tempoFinal = clock();
+	tempoTotal = (tempoFinal - tempoInicial)/(CLOCKS_PER_SEC);	
+	printf("Tempo de ordenacao: %.12f\nnumero de operacoes:%d\n--\n", tempoTotal, aux);
+	aux = 0;
 	
-	for (j=0 ; j<i ; j++){
-		printf("%s, %ld, %ld, %ld\n", paises[j].pais, paises[j].area, paises[j].pop, paises[j].gdp);
-	}
+	printf("2.b.b -  Quick Sort - Area (pivo aleatorio):\n");
+	tempoInicial = clock();
+	quickSortPivoAleatorioArea(paises,0,tam,&aux);
+	tempoFinal = clock();
+	tempoTotal = (tempoFinal - tempoInicial)/(CLOCKS_PER_SEC);	
+	printf("Tempo de ordenacao: %.12f\nnumero de operacoes:%d\n--\n", tempoTotal, aux);
+	aux = 0;
+		
+	printf("2.b.c -  Quick Sort - Area (pivo mediana):\n");
+	tempoInicial = clock();
+	quickSortPivoMedianaArea(paises,0,tam,&aux);
+	tempoFinal = clock();
+	tempoTotal = (tempoFinal - tempoInicial)/(CLOCKS_PER_SEC);	
+	printf("Tempo de ordenacao: %.12f\nnumero de operacoes:%d\n--\n", tempoTotal, aux);
+	aux = 0;
+
+	//NOME
+	printf("2.c.a -  Quick Sort - Nome (pivo meio):\n");
+	tempoInicial = clock();
+	quickSortPivoMeioNome(paises,0,tam,&aux);
+	tempoFinal = clock();
+	tempoTotal = (tempoFinal - tempoInicial)/(CLOCKS_PER_SEC);	
+	printf("Tempo de ordenacao: %.12f\nnumero de operacoes:%d\n--\n", tempoTotal, aux);
+	aux = 0;
 	
+	printf("2.c.b -  Quick Sort - Nome (pivo aleatorio):\n");
+	tempoInicial = clock();
+	quickSortPivoAleatorioNome(paises,0,tam,&aux);
+	tempoFinal = clock();
+	tempoTotal = (tempoFinal - tempoInicial)/(CLOCKS_PER_SEC);	
+	printf("Tempo de ordenacao: %.12f\nnumero de operacoes:%d\n--\n", tempoTotal, aux);
+	aux = 0;
+		
+	printf("2.c.c -  Quick Sort - Nome (pivo mediana):\n");
+	tempoInicial = clock();
+	quickSortPivoMedianaNome(paises,0,tam,&aux);
+	tempoFinal = clock();
+	tempoTotal = (tempoFinal - tempoInicial)/(CLOCKS_PER_SEC);	
+	printf("Tempo de ordenacao: %.12f\nnumero de operacoes:%d\n--\n", tempoTotal, aux);
+	aux = 0;
+
+
+
+	//3.a - Heap Sort:
+	printf("1.a. Heap Sort - Area.\n");
+	tempoInicial = clock();
 	heapArea(paises, i+1, &contArea);
+	tempoFinal = clock();
+	tempoTotal = (tempoFinal - tempoInicial)/(CLOCKS_PER_SEC);	
+	printf("Tempo de ordenacao: %.12f\nnumero de operacoes:%d\n--\n", tempoTotal, contArea);
+	contArea = 0;
 
+	printf("1.a. Heap Sort - Populacao.\n");
+	tempoInicial = clock();
 	heapPop(paises, i+1, &contPop);
+	tempoFinal = clock();
+	tempoTotal = (tempoFinal - tempoInicial)/(CLOCKS_PER_SEC);	
+	printf("Tempo de ordenacao: %.12f\nnumero de operacoes:%d\n--\n", tempoTotal, contPop);
+	contPop=0;
 
+	printf("1.a. Heap Sort - GDP.\n");
+	tempoInicial = clock();
 	heapGdp(paises, i+1, &contGdp);
+	tempoFinal = clock();
+	tempoTotal = (tempoFinal - tempoInicial)/(CLOCKS_PER_SEC);	
+	printf("Tempo de ordenacao: %.12f\nnumero de operacoes:%d\n--\n\n\n", tempoTotal, contGdp);
+	contGdp=0;
 
-	for (j=0 ; j<i ; j++){
-		printf("%ld\n", paises[j].gdp);
-	}
-	printf("\n\ncontArea = %d\ncontPop = %d\ncontGdp = %d", contArea, contPop, contGdp);
-	
 
+heapPop(paises, i+1, &contPop);
+/*
+//Busca Sequencial
+printf("BUSCA SEQUENCIAL\nchave pop:\n");
+scanf("%ld", &contPop);
+
+tempoInicial = clock();
+buscaSequencialPop(paises, contPop, tam, &aux);
+tempoFinal = clock();
+tempoTotal = (tempoFinal - tempoInicial)/(CLOCKS_PER_SEC);	
+printf("Tempo de busca: %.12f\nnumero de operacoes:%d\n--\n\n\n", tempoTotal, aux);
+aux = 0;
+
+
+// BUSCA INTERPOLADA
+	printf("\n\n== BUSCAS INTERPOLADAS ==\n");
+	printf("Chave GDP: ");
+	scanf("%ld",&chaveGdp);
+
+	tempoInicial = clock();
+	aux = buscaInterpolaPop(paises,chaveGdp,0,tam,&aux);
+	tempoFinal = clock();
+	tempoTotal = (tempoFinal - tempoInicial)/(CLOCKS_PER_SEC);	
+	printf("Tempo de busca: %.12f\nnumero de operacoes:%d\n--\n\n\n", tempoTotal, aux);
+*/
+
+csortGdp(*paises, i+1, contGdp);
+
+	free(paises);
+	fclose(fp);
 	return 0;
 }
--- icc2/buda/trab.c
+++ icc2/buda/trab.c
@@ -1,10 +1,9 @@
 #include "trab.h"
-/*
-typedef struct{
-	char pais[30];
-	long area, gdp, pop;
-} no;
-*/
+
+
+//--------------------------------------------------------------------
+// FUNCOES HEAP SORT
+//--------------------------------------------------------------------
 
 void heapArea(no *paises, int tam, int *cont){
 	int i;
@@ -34,7 +33,6 @@ void heapArea(no *paises, int tam, int *cont){
 		heapifyArea(paises, 0, i-1, cont);
 	}
 }
-
 void heapifyArea(no *paises, int raiz, int fundo, int *cont){
 	int rdy, filhoMax;
 	no aux;
@@ -79,8 +77,6 @@ void heapifyArea(no *paises, int raiz, int fundo, int *cont){
 		}
 	}
 }
-
-
 void heapPop(no *paises, int tam, int *cont){
 	int i;
 	no aux;
@@ -106,10 +102,9 @@ void heapPop(no *paises, int tam, int *cont){
 
 		*cont = *cont+13;
 
-		heapifyArea(paises, 0, i-1, cont);
+		heapifyPop(paises, 0, i-1, cont);
 	}
 }
-
 void heapifyPop(no *paises, int raiz, int fundo, int *cont){
 	int rdy, filhoMax;
 	no aux;
@@ -128,7 +123,7 @@ void heapifyPop(no *paises, int raiz, int fundo, int *cont){
 			*cont = *cont + 1;
 		}
 
-		if(paises[raiz].area < paises[filhoMax].area){
+		if(paises[raiz].pop < paises[filhoMax].pop){
 			aux.pop = paises[raiz].pop;
 			aux.gdp = paises[raiz].gdp;
 			aux.area = paises[raiz].area;
@@ -154,12 +149,11 @@ void heapifyPop(no *paises, int raiz, int fundo, int *cont){
 		}
 	}
 }
-
 void heapGdp(no *paises, int tam, int *cont){
 	int i;
 	no aux;
 	for(i = (tam/2) ; i>=0 ; i--){
-		heapifyPop(paises, i, tam-1, cont);
+		heapifyGdp(paises, i, tam-1, cont);
 		*cont = *cont+1;
 	}
 	for(i = tam-1 ; i>=1 ; i--){
@@ -180,10 +174,9 @@ void heapGdp(no *paises, int tam, int *cont){
 
 		*cont = *cont+13;
 
-		heapifyArea(paises, 0, i-1, cont);
+		heapifyGdp(paises, 0, i-1, cont);
 	}
 }
-
 void heapifyGdp(no *paises, int raiz, int fundo, int *cont){
 	int rdy, filhoMax;
 	no aux;
@@ -202,7 +195,7 @@ void heapifyGdp(no *paises, int raiz, int fundo, int *cont){
 			*cont = *cont + 1;
 		}
 
-		if(paises[raiz].area < paises[filhoMax].area){
+		if(paises[raiz].gdp < paises[filhoMax].gdp){
 			aux.pop = paises[raiz].pop;
 			aux.gdp = paises[raiz].gdp;
 			aux.area = paises[raiz].area;
@@ -227,4 +220,307 @@ void heapifyGdp(no *paises, int raiz, int fundo, int *cont){
 			rdy=1;
 		}
 	}
+}
+void heapNome(no *paises, int tam, int *cont){
+	int i;
+	no aux;
+	for(i = (tam/2) ; i>=0 ; i--){
+		heapifyNome(paises, i, tam-1, cont);
+		*cont = *cont+1;
+	}
+	for(i = tam-1 ; i>=1 ; i--){
+		aux.pop = paises[0].pop;
+		aux.gdp = paises[0].gdp;
+		aux.area = paises[0].area;
+		strcpy(aux.pais, paises[0].pais);
+
+		paises[0].pop = paises[i].pop;
+		paises[0].gdp = paises[i].gdp;
+		paises[0].area = paises[i].area;
+		strcpy(paises[0].pais, paises[i].pais);
+
+		paises[i].pop = aux.pop;
+		paises[i].gdp = aux.gdp;
+		paises[i].area = aux.area;
+		strcpy(paises[i].pais, aux.pais);
+
+		*cont = *cont+13;
+
+		heapifyNome(paises, 0, i-1, cont);
+	}
+}
+void heapifyNome(no *paises, int raiz, int fundo, int *cont){
+	int rdy, filhoMax;
+	no aux;
+	rdy = 0;
+	while((raiz*2) <= fundo && (!rdy)) {
+		if(raiz*2 == fundo){
+			filhoMax = raiz*2;
+			*cont = *cont + 1;
+		}//		paises[raiz*2].pais							       > 		paises[raiz*2+1].pais
+		else if(strcmp(paises[raiz*2].pais, paises[raiz*2+1].pais) > strcmp(paises[raiz*2+1].pais, paises[raiz*2].pais)){
+			filhoMax = raiz*2;
+			*cont = *cont + 1;
+		}
+		else{
+			filhoMax = raiz*2 + 1;
+			*cont = *cont + 1;
+		}
+		//				paises[raiz].pais 				   <                   paises[filhoMax].pais)
+		if(strcmp(paises[raiz].pais, paises[filhoMax].pais) < strcmp(paises[filhoMax].pais, paises[raiz].pais)){
+			aux.pop = paises[raiz].pop;
+			aux.gdp = paises[raiz].gdp;
+			aux.area = paises[raiz].area;
+			strcpy(aux.pais, paises[raiz].pais);
+
+			paises[raiz].pop = paises[filhoMax].pop;
+			paises[raiz].gdp = paises[filhoMax].gdp;
+			paises[raiz].area = paises[filhoMax].area;
+			strcpy(paises[raiz].pais, paises[filhoMax].pais);
+			
+			paises[filhoMax].pop = aux.pop;
+			paises[filhoMax].gdp = aux.gdp;
+			paises[filhoMax].area = aux.area;
+			strcpy(paises[filhoMax].pais, paises[raiz].pais);
+			
+			*cont = *cont + 13;
+
+			raiz = filhoMax;
+		}
+		else{
+			*cont = *cont + 1;
+			rdy=1;
+		}
+	}
+}
+
+//--------------------------------------------------------------------
+// FUNCOES BUSCA SEQUENCIAL
+//--------------------------------------------------------------------
+//Pesquisa População/Area/Nome do pais sequencialmente. Retorna indice caso encontrou ou -1 se nao encontrar.
+int buscaSequencialPop(no paises[],long chavePop, int tam, int* cont){
+	int i=0;
+	*cont = *cont+1;
+	
+	for(;i<tam;i++){
+		if(paises[i].pop == chavePop){
+			*cont += i*3+1;
+			return i;
+		}
+	}
+	*cont += i*3+1;	
+return -1;
+}
+int buscaSequencialArea(no paises[],long chaveArea, int tam, int* cont){
+	int i=0;
+	*cont = *cont+1;
+	
+	for(;i<tam;i++){
+		if(paises[i].area == chaveArea){
+			*cont += i*3+1;	
+			return i;
+		}
+	}
+	*cont += i*3+1;		
+return -1;
+}
+int buscaSequencialNome(no paises[],char chaveNome[], int tam,int* cont){
+	int i=0;
+	*cont = *cont+1;
+	for(;i<tam;i++){
+		if(strcmp(paises[i].pais,chaveNome)==0){
+			*cont += i*3+1;	
+			return i;
+		}
+	}
+	*cont += i*3+1;	
+return -1;
+}
+
+//--------------------------------------------------------------------
+// FUNCOES BUSCA BINARIA
+//--------------------------------------------------------------------
+//Pesquisa binaria de População/Area/Nome. Retorna indice caso encontrar ou -1 se não encontrar.
+int buscaBinariaPop(no paises[],long chavePop, int ini, int fim,int* cont){
+	int i;
+	*cont+=2;
+	i = (ini + fim)/2;
+	
+	if(paises[i].pop==chavePop){
+		*cont+=2;
+		return i;
+	}else if(ini == fim){
+		*cont+=2;
+		return -1;
+	}
+	
+	else if(paises[i].pop > chavePop){
+		*cont = *cont+3;
+		return	buscaBinariaPop(paises,chavePop,ini, i-1,cont);
+	}else if(paises[i].pop < chavePop){
+		*cont = *cont+3;
+		return buscaBinariaPop(paises,chavePop,i+1,fim,cont);
+	}
+	*cont = *cont+1;
+	return -1;
+}
+int buscaBinariaArea(no paises[],long chaveArea, int ini, int fim,int* cont){
+	int i;
+	*cont+=2;
+	i = (ini + fim)/2;
+	
+	if(paises[i].area==chaveArea){
+		*cont+=2;
+		return i;
+	}else if(ini == fim){
+		*cont+=2;
+		return -1;
+	}
+	
+	else if(paises[i].area > chaveArea){
+		*cont = *cont+3;
+		return	buscaBinariaArea(paises,chaveArea,ini, i-1,cont);
+	}else if(paises[i].area < chaveArea){
+		*cont = *cont+3;
+		return buscaBinariaArea(paises,chaveArea,i+1,fim,cont);
+	}
+	
+	*cont = *cont+1;
+	return -1;
+}
+int buscaBinariaNome(no paises[],char chaveNome[], int ini, int fim,int* cont){
+	int i;
+	
+	i = (ini + fim)/2;
+	*cont+=2;
+	
+	if(strcmp(paises[i].pais,chaveNome)==0){
+		*cont+=2;
+		return i;
+	}else if(ini == fim){
+		*cont+=2;
+		return -1;
+	}
+	
+	else if(strcmp(paises[i].pais,chaveNome) > 0){
+		*cont = *cont+3;
+		return	buscaBinariaNome(paises,chaveNome,ini, i-1,cont);
+	}else if(strcmp(paises[i].pais,chaveNome) < 0){
+		*cont = *cont+3;
+		return buscaBinariaNome(paises,chaveNome,i+1,fim,cont);
+	}
+	
+	*cont = *cont+1;
+	return -1;
+}
+
+//--------------------------------------------------------------------
+// FUNCOES BUSCA POR INTERPOLACAO
+//--------------------------------------------------------------------
+//Pesquia por interpolação por População/Area/Nome
+int buscaInterpolaPop(no *paises,long chavePop, int ini, int fim,int* cont){
+	int meio;
+	
+	while(ini<=fim){
+
+		meio = ini + ((fim-ini)*(chavePop-paises[ini].pop) / (paises[fim].pop - paises[ini].pop));
+		*cont = *cont+8;
+			
+		if(chavePop < paises[meio].pop){
+			*cont = *cont+3;
+			fim = meio-1;
+		}else if(chavePop > paises[meio].pop){
+			*cont = *cont+3;			
+			ini = meio+1;
+		}else{
+			*cont = *cont+1;
+			return meio;
+		}
+	}
+	*cont = *cont+1;
+	return -1;
+}
+int buscaInterpolaArea(no *paises,long chaveArea, int ini, int fim,int* cont){
+    int meio;
+    
+    while(ini <= fim){
+    	
+        meio = ini + ((fim-ini)*((chaveArea-paises[ini].area) / (paises[fim].area - paises[ini].area)));
+        *cont = *cont+8;
+        
+        if(chaveArea < paises[meio].area){
+			*cont = *cont+3;		
+            fim = meio - 1;
+        }else if(chaveArea > paises[meio].area){
+        	*cont = *cont+3;
+            ini = meio + 1;
+        }else{
+        	*cont = *cont+1;
+            return meio;
+    	}
+	}
+	
+	*cont = *cont+1;
+    return -1;
+}
+int buscaInterpolaGdp(no *paises,long chaveGdp, int ini, int fim,int* cont){
+    int meio;
+    
+    while(ini<=fim){
+    	
+        meio = ini + ((fim-ini)*((chaveGdp-paises[ini].gdp) / (paises[fim].gdp - paises[ini].gdp)));
+        *cont = *cont+8;
+        
+        if(chaveGdp < paises[meio].gdp){
+        	*cont = *cont+3;
+            fim = meio - 1;
+        }else if(chaveGdp > paises[meio].gdp){
+        	*cont = *cont+3;
+            ini = meio + 1;
+        }else{
+        	*cont = *cont+1;
+            return meio;
+    	}
+	}
+	
+	*cont = *cont+1;
+    return -1;
+}
+
+
+
+
+
+
+void csortGdp(no *paises, int tam, int *cont){
+    long maior=0, i;
+    no aux;
+
+    for(i=0 ; i<tam; i++){
+        if(maior < paises[i].gdp)
+        maior = paises[i].gdp;
+    }
+    no *vet2, *vet3;
+    
+    vet3 = (no*) calloc (tam, sizeof(no));
+    vet2 = (no*) calloc (maior+1, sizeof(no));
+
+    for(i=0 ; i<tam; i++){
+        vet2[paises[i].gdp] = vet2[paises[i].gdp].gdp + 1;
+    }
+    //POSSIVEIS CAGOS
+    for(i=0 ; i<maior+1 ; i++){
+        aux.gdp = aux.gdp + vet2[i]gdp;
+        vet2[i].gdp = aux.gdp;
+    }
+    //LOTSA CAGOS
+    for(i=tam-1 ; i>=0 ; i--){
+        vet3[vet2[paises[i]]-1].gdp = paises[i].gdp;
+        --vet2[paises[i]];
+    }
+    for(i=0 ; i<N ; i++){
+        printf("%d ", vet3[i].gdp);
+    }
+
+
 }
