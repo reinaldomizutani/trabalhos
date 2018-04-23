9a46049643a1632a8321c304415c9c73b95109d4
Reinaldo Mizutani <30627739+reinaldomizutani@users.noreply.github.com>
2018-04-23 16:58:25 -0300

    Update clovis.cpp

--- itc/clovis.cpp
+++ itc/clovis.cpp
@@ -1,3 +1,11 @@
+/*
+Introdução a Teoria da Computação - turma 2018
+Alunos:
+Hugo de Azevedo Vitulli  			10295221
+Matheus Vinicius Gouvea de Godoi  	10295217
+Reinaldo Mizutani					7062145
+*/
+
 #include <vector>
 #include <stdlib.h>
 #include <stdio.h>
@@ -23,31 +31,73 @@ Grafo iniciaGrafo(int nVert){
 	return g;
 }
 
+//Adcionar aresta no grafo
 void addAresta(Grafo *g, int noV, int noW, char peso){
 	g->listaAdj[noV].push_back(make_pair(noW, peso));
 }
 
+//Função para imprimir grafo 
 void printGrafo(Grafo g){
 	for(unsigned int i = 0; i < g.listaAdj.size(); i++){
-		printf("[%d] -> ",i);
+		printf("[%d]\n",i);
 		for(unsigned int j = 0; j < g.listaAdj[i].size(); j++){
-			printf("[w=%d, p=%c]",g.listaAdj[i][j].first, g.listaAdj[i][j].second);
+			printf("%d %c\n",g.listaAdj[i][j].first, g.listaAdj[i][j].second);
 		}
-		printf("\n");
 	}
 }
 
-bool checaFrase(Grafo g, int vertAtual, char frase[]){
+bool checaFrase(Grafo g, int vertAtual, char frase[], char terminal[], char aceitacao[]){
+	
+	//checar se a cadeia tem terminais válidos somente
+	for (unsigned int i = 0; i < strlen(frase); i++){				//Pra todo caracter da cadeia
+		int flagTerminalExiste = 0;
+		for(unsigned int j = 0; j < strlen(terminal); j++){				//Pra todo terminal
+			if(frase[i]==terminal[j]){								//Se encontrar um terminal igual, ele existe
+				flagTerminalExiste = 1;
+				break;
+			}
+		}
+		if(!flagTerminalExiste){							//Caso não encontre, já não é possivel aceitar
+			return false;
+		}
+	}
 
+
+	//checa a cadeia no automato
+	for (unsigned int i = 0; i < strlen(frase); i++)	{				//Pra todo caracter da cadeia
+		int flagEncontrouPeso = 0;
+		for (unsigned int j = 0; j < g.listaAdj[vertAtual].size(); j++){			//Pra toda ligação de cada estado
+			if(g.listaAdj[vertAtual][j].second == '#'){						//Remove auxiliares
+				continue;
+			}
+			if(g.listaAdj[vertAtual][j].second == frase[i]){			//Se encontrar uma ligacao
+				vertAtual = g.listaAdj[vertAtual][j].first;					//Usa essa ligacao como qX atual para continuar processando
+				flagEncontrouPeso = 1;
+				break;
+			}
+		}
+		if(!flagEncontrouPeso){								//Caso não encontre, cadeia é inválida
+			return false;
+		}
+	}
+
+	//checa estados de aceitacao
+	for(unsigned int j = 0; j < strlen(aceitacao); j++){			//Para todo estado de aceitação
+		if(aceitacao[j] == ' '){							//Ignora o espaço no meio do vetor
+			continue;
+		}
+		if(vertAtual == (aceitacao[j] - 48)){				//Caso o vertice que parou seja existente na aceitação, a cadeia é válida
+			return true;
+		}
+	}
+	return false;
 }
 
 int main(){
-	//----------------------------------
-	// declaração de variáveis
-	//----------------------------------
+	//Todas variaveis utilizadas
 	int nVert = 0;
 	char vetorTerminais[MAX];
-	int estadosIniciais = 0;
+	char estadosIniciais[MAX];
 	char vetorAceitacao[MAX];
 	char fraseTestar[MAX];
 	int numArestas = 0;
@@ -57,84 +107,64 @@ int main(){
 	int vertAtual = 0;
 	char peso = 0;
 	bool flagFrase = false;
+	int contadorSaida = 1;
 	Grafo g;
 
-//Programa em si:
-	//----------------------------------
-	// linha 1: número de estados.
-	//----------------------------------
+	//Número de estados do grafo
 	scanf("%d\n",&nVert);
-	g = iniciaGrafo(nVert); // inicia o grafo do autômato.
-	
-	//----------------------------------
-	// linha 2: conjunto de símbolos terminais.
-	//----------------------------------
+	g = iniciaGrafo(nVert);
+
+	//Vetor de caracteres terminais
 	scanf("%[^\n]s",vetorTerminais);
+	getchar();
 
-	//----------------------------------
-	// linha 3: número de estados iniciais:
-	// 		1 se é Automato Finito deterministico, 
-	//		0 1 2 3 ou 0,1,2,3 se é Automato Finito Nao deterministico.
-	//----------------------------------
-	scanf("%d\n",&estadosIniciais);
-
-
-
-
-
-	// não entendi -> joga o vértice (0, -1, com peso i)
-	for(int i = 0; i < estadosIniciais; i++){
-		addAresta(&g, i, -1, 'i');
+	//Vetor dos estados Iniciais
+	scanf("%[^\n]s",estadosIniciais);
+	getchar();
+	//Para todos iniciais, inicia o estado com uma ligação para o estado -1, de peso 'i', para sabermos que é inicial
+	for(unsigned int i = 0; i < strlen(estadosIniciais); i++){
+		if(estadosIniciais[i] == ' '){
+			continue;
+		}
+		addAresta(&g, estadosIniciais[i]-48, -1, 'i');
 	}
-	for(int i = estadosIniciais; i < nVert; i++){
+	//Os que não são iniciais, recebem uma auxiliar -1, '#', para completar as primeiras ligações de cada estado
+	for(int i = strlen(estadosIniciais); i < nVert; i++){
 		addAresta(&g, i, -1, '#');
 	}
-	//----------------------------------
-	// linha 4: conjunto de estados de aceitação:
-	// 		depois da cadeia ser processada, se o autômato estiver em um dos 
-	// 		nós desse conjunto, a frase é considerada "aceita".
-	//----------------------------------
+
+	//Vetor de aceitação
 	scanf("%[^\n]s",vetorAceitacao);
+	getchar();
 
-
-	//----------------------------------
-	// linha 5: número de transições -> número de arestas no grafo.
-	//----------------------------------
+	//Número de ligações entre estados
 	scanf("%d\n",&numArestas);
 
-
-	//----------------------------------
-	// linha 6: todas as transições -> todas as arestas do grafo, listadas no formato (v w peso).
-	//----------------------------------
-	for (int i = 0; i < numArestas; i++){
+	for (int i = 0; i < numArestas; i++){			//Cada ligação entre estados é uma nova aresta no grafo
 		scanf("%d %c %d\n",&noV, &peso, &noW);
 		addAresta(&g, noV, noW, peso);	
 	}
-	printGrafo(g);
 
-	//----------------------------------
-	// linha abaixo das transições: número de casos de teste
-	//----------------------------------
+	//Número de frases para testar
 	scanf("%d\n",&numTestes);
-
-	//----------------------------------
-	// proxima linha: verificando se a frase pode ser produzida pelo grafo
-	//----------------------------------
-	for(int i = 0; i < numTestes; i++){
-		scanf("%[^\n]s",fraseTestar);
+	for(int i = 0; i < numTestes; i++){			//Para cada frase de teste
+		scanf("%[^\n]s",fraseTestar);				//Pegar a frase
+		getchar();
 		flagFrase = false;
-		for(int j = 0; j < g.nVert; j++){
-			if(g.listaAdj[j][0].first == 'i'){
+		for(int j = 0; j < g.nVert; j++){		//Para todo nó no grafo
+			if(g.listaAdj[j][0].second == 'i'){		//Checar os iniciais, e chamar a função com TODOS iniciais
 				vertAtual = j;
-				flagFrase = checaFrase(g, vertAtual, fraseTestar);
+				flagFrase = checaFrase(g, vertAtual, fraseTestar, vetorTerminais, vetorAceitacao);
 				if(flagFrase){
-					printf("aceita\n");
+					printf("%d. aceita\n",contadorSaida);				//Caso um inicial tenha resposta 'aceita', não é presciso checar os outros
+					contadorSaida++;
 					break;
 				}
 			}
 		}
-		if(!flagFrase){
-			printf("rejeita\n");
+		if(!flagFrase){							//Caso nenhum inicial tenha tido 'aceita', então a frase é rejeitada
+			printf("%d. rejeita\n",contadorSaida);
+			contadorSaida++;
 		}
 	}
 
