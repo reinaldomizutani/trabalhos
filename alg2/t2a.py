aa024525a2e9cdbc5bffa37e13ea7f6b4bfb8bb1
Reinaldo Mizutani <30627739+reinaldomizutani@users.noreply.github.com>
2018-05-27 11:03:24 -0300

    Update t2a.py

--- alg2/t2a.py
+++ alg2/t2a.py
@@ -54,41 +54,121 @@ def insere(ndx):
 			tempNdx = format(tempNdx, "05d")
 			linhaNdx = nro + ' ' + tempNdx
 			ndxFile.write(linhaNdx)
-			print(ndx)
 			return ndx
-
 
 def remove(ndx):
 	remover = input("digite o numero a ser removido: ")
 	remover = int(remover)
+	contLines = 0
 	with open('Dados.txt', 'r+') as f:
-		if(remover in ndx):
-			numero = ndx[remover]
-			f.seek(numero)
-			f.write('#')
-			ndx.pop(numero, None)
+		with open('primario.ndx', 'r+')as primario:
+			if(remover in ndx):
+				numero = ndx[remover]
+				f.seek(numero) #numero é a posicao no "dados.txt"
+				f.write('#')
 
-
-			### QUE???? TO FAZENDO CAGADA AQUI!!!
-			with open('primario.ndx', 'r+')as primario:
-				tempNdx = primario.readlines()
-				for line in tempNdx:
+				ndxFile = primario.readlines()
+				for line in ndxFile:
+					contLines += 1
 					nro, pos = line.split()
-					nro = int(nro)
-					if(nro == remover):
-						pos = int(pos)
-						primario.seek(pos)
-						primario.write("#")
-						print('registro {} removido com sucesso'.format(remover))
+					try:
+						if(int(nro) == remover):
+							primario.seek((contLines-1) * 11)
+							primario.write('#')
+							print("Remocao realizada com sucesso! ")
+					except Exception as e:
+						pass
 					
-
+				
+				return ndx
+				# FAZER DEPOIS!!!! ndx.pop(numero, None)
+			else:
+				print("remocao não realizada: numero nao consta no arquivo.")
+				return ndx
 
 def altera(ndx):
-	print(ndx)
+	
+	nro = int(input('digite o numero da cada que voce quer alterar: '))
+	if nro in ndx:
+		opt = 0
+		while(opt>3 or opt<1):
+			print('qual dado você quer alterar:')
+			print('1) numero ')
+			print('2) nome')
+			print('3) veiculo')
+			opt = int(input('opcao: '))
 
+		value = input('digite o novo valor: ')
+
+		if(opt == 1):
+			if(int(value) in ndx):
+				print("ERRO: Numero de registro duplicado! ")
+
+		with open('dados.txt', 'r+') as dados:
+			with open('primario.ndx', 'r+') as primario:
+				posicao = ndx[nro]
+				dados.seek(posicao)
+				linha = dados.readline()
+				nro, nome, veiculo = linha.split('|')
+				novalinha = ''
+				if(opt == 1):
+					value = int(value)
+					novalinha = str(format(value, '03d')) + '|' + nome + '|' + veiculo
+				elif(opt == 2):
+					novalinha = nro + '|' + value.ljust(40, ' ') + '|' + veiculo
+				elif(opt == 3):
+					novalinha = nro + '|' + nome + '|' + value.ljust(20, ' ')
+
+				dados.seek(posicao)
+				dados.write(novalinha)
+		return ndx
+	else: 
+		print("numero digitado não consta no banco de dados. ")
+		return ndx
+	
 def compacta(ndx):
-	print(ndx)
+	newFile = []
+	with open('dados.txt', 'r') as dados:
+		dadosMem = dados.readlines()
+		for line in dadosMem:
+			
+			nro, nome, veiculo = line.split('|')
+			if(nro[0] == '#'):
+				pass
+			else:
+				veiculo = veiculo.rstrip('\n')
+				newLine = [nro, nome, veiculo]
+				newFile.append(newLine)
+		
+		newFile = sorted(newFile)
+	
+	with open("dados.txt", "w")as newDados:
+		with open("primario.ndx", 'w') as primario:
+			ndxMem = {}
+			finalposition = 0
+			for item in newFile:
+				tempLine = item[0] + '|' + item[1] + '|' + item[2] + '\n'
+				primario.write(item[0] + ' ' + str(format(finalposition, '05d')) + '\n')
+				ndxMem[int(nro)] = finalposition
+				finalposition = finalposition + len(tempLine) + 1
+				newDados.write(tempLine)
 
+
+	return ndx
+
+def procura(ndx):
+	numero = int(input('numero a ser buscado: '))
+
+	if numero in ndx:
+		indice = ndx[numero]
+		print('usuario encontrado com sucesso!')
+		with open('dados.txt', 'r') as f:
+			f.seek(indice,0)
+			linha = f.readline()
+			print(linha)
+	else:
+		print('Usuario nao encontrado...')
+	print('\n')
 
 if __name__ == '__main__':
 	ndx = leTabela()
