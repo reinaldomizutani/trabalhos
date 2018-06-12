1313abef0bf4c779056504e2373844a584789d8d
Reinaldo Mizutani <30627739+reinaldomizutani@users.noreply.github.com>
2018-06-12 17:53:23 -0300

    Add files via upload

---
+++ itc/oldclovis.py
@@ -0,0 +1,98 @@
+'''
+Introdução a Teoria da Computação - turma 2018
+Discentes:
+Hugo de Azevedo Vitulli  			10295221
+Matheus Vinicius Gouvea de Godoi  	10295217
+Reinaldo Mizutani					7062145
+'''
+def testeCadeias(graph, estadosIniciais, cadeias, aceitacao, terminais):
+
+	#para todos os estados iniciais
+	for estado in estadosIniciais:
+
+		noAtual = estadosIniciais[0]-1
+
+		#para cada letra da cadeia
+		for letra in cadeia:
+			if(letra not in terminais):
+				return 0
+
+			if(letra not in [item[1] for item in graph[noAtual]]):
+				return 0
+
+			for adj in graph[noAtual]:
+				if(adj[1] == letra):
+					noAtual = adj[0]
+
+	
+	if(noAtual in aceitacao):
+		return 1
+	else:
+		return 0
+		
+
+
+if __name__ == '__main__':
+	
+
+	graph = {}
+	transicoes = []
+	cadeias = []
+	with open('1.in', 'r') as f:
+		#linha 1
+		estados = f.readline()
+		estados = int(estados)	
+
+		#linha 2
+		terminais = f.readline()
+		terminais = terminais.split()		
+
+		#linha 3
+		estadosIniciais = f.readline()
+		estadosIniciais = estadosIniciais.split()
+		for item in estadosIniciais:
+			a = int(item)
+			estadosIniciais.remove(item)
+			estadosIniciais.append(a)		
+
+		#linha 4
+		aceitacao = f.readline()
+		aceitacao = [int(n) for n in aceitacao.split()]		
+
+		#linha 5
+		numeroTransicoes = f.readline()
+		numeroTransicoes = int(numeroTransicoes)		
+
+		for x in range(int(numeroTransicoes)):
+			x = f.readline()
+			transicoes.append(x.rstrip('\n'))
+
+		# numero de cadeias
+		numeroCadeias = int(f.readline())		
+
+		# cadeias
+		for x in range(numeroCadeias):
+			x = f.readline()
+			x = x.rstrip('\n')
+			temp = list(x)
+			cadeias.append(temp)
+
+	estados = int(estados)
+	for x in range(estados):
+		graph.setdefault(x, [])	
+
+	for item in transicoes:
+		v, peso, w = item.split()
+		v = int(v)
+		w = int(w)
+		temp = [w, peso]
+		graph[v].append(temp)
+	
+	a = 1
+	for cadeia in cadeias:
+		result = testeCadeias(graph, estadosIniciais, cadeias, aceitacao, terminais)
+		if(result == 1):
+			print(str(a) + '. aceita')
+		else:
+			print(str(a) + '. rejeita')
+a +=1
---
+++ itc/proBackup.py
@@ -0,0 +1,138 @@
+def leArquivo():
+	with open('entrada.txt', 'r') as file:
+		#linha 1: (int) estados
+		estados = int(file.readline())
+
+		#linha 2: (lista) terminais
+		terminais = file.readline()
+		terminais = terminais[2:]
+		terminais = terminais.split()
+
+		#linha 3: (lista) naoTerminais
+		naoTerminais = file.readline()
+		naoTerminais = naoTerminais[2:]
+		naoTerminais = naoTerminais.split()
+
+		#linha 4: (int) aceitacao
+		aceitacao = int(file.readline())
+
+
+		#linha 5: (int) numero de transicoes
+		numTransicoes = int(file.readline())
+
+		#linha 6 a (numTransicoes + 6): (matriz de listas) definição das arestas do grafo que representa a maquina de turing
+		transicoes = []
+		for linha in range(numTransicoes):
+			temp = file.readline()
+			temp = temp.split()
+			transicoes.append(temp)
+
+		#num cadeias a serem testadas: (int) numero de cadeias
+		numCadeias = int(file.readline())
+
+		#cadeias a serem testadas: (lista) lista com as cadeias a serem testadas
+		cadeias = []
+		for linha in range(numCadeias):
+			temp = file.readline()
+			cadeias.append(temp.rstrip('\n'))
+	
+		return estados, terminais, naoTerminais, aceitacao, transicoes, cadeias
+
+def printaCoisas(estados, terminais, naoTerminais, aceitacao, transicoes, cadeias):
+
+	print('estados = ')
+	print(str(estados))
+	print(' ')
+
+	print('terminais : ')
+	print(terminais)
+	print(' ')
+
+	print("não serminais (sigma'): ")
+	print(naoTerminais)
+	print(' ')
+
+	print('aceitacao = ')
+	print(str(aceitacao))
+	print(' ')
+
+	print('transicoes: ')
+	print(transicoes)
+	print(' ')
+
+	print('cadeias :')
+	print(cadeias)
+	print(' ')
+
+def preparaGrafo(estados, transicoes):
+	graph = {}
+	for x in range(estados):
+		graph.setdefault(x,[])
+
+	for item in transicoes:
+		key = int(item[0])
+		tempTransicoes = item[1:]
+		graph[key].append(tempTransicoes)
+
+	return graph
+
+def testeCadeias(graph, posAtual, cadeia, estado):
+
+	resultado = 0
+	if estado == 5:
+		return 1
+
+	else:
+		for lista in graph[estado]:
+
+			#se existe instrucao para o que esta na cabeca de leitura:
+			if(cadeia[posAtual] in lista): 
+				newEstado = int(lista[1])
+				escrever = lista[2]
+				movePara = lista[3]
+			else:
+				print("não achou na lista")
+				continue
+				
+			newCadeia = cadeia[:posAtual] + escrever + cadeia[posAtual+1:]
+			if(movePara == 'R'):
+				newPosicao = posAtual + 1
+			elif(movePara == 'L'):
+				newPosicao = posAtual -1
+			else:
+				newPosicao = posAtual
+
+			print("estado = " + str(estado))
+			print('cadeia = ' + cadeia)
+			print('posicao atual = ' + str(posAtual))
+			print("move Para = " + movePara)
+			print('nova cadeia = ' + newCadeia)
+			print('novo estado = ' + str(newEstado))
+			print('--------------')
+
+
+			resultado = testeCadeias(graph, newPosicao, newCadeia, newEstado)
+			
+	return resultado
+
+if __name__ == '__main__':
+
+	# ----------------------- leitura e preparacao dos dados: ----------------
+	estados, terminais, naoTerminais, aceitacao, transicoes, cadeias = leArquivo()
+
+	#printaCoisas(estados, terminais, naoTerminais, aceitacao, transicoes, cadeias)
+
+	graph = preparaGrafo(estados, transicoes)
+
+	# ------------------------ começo do codigo em si ------------------------
+	estado = 0
+	posAtual = 1
+	novaCadeia = ' ' + cadeias[0] + ' '
+	resultado = testeCadeias(graph, posAtual, novaCadeia, estado)
+	"""
+	for cadeia in cadeias:
+		novaCadeia = ' ' + cadeia + ' '
+		resultado = testeCadeias(graph, posAtual, novaCadeia, estado)
+		if resultado == 1:
+			print("DEU CERTO PORRA")
+	"""
